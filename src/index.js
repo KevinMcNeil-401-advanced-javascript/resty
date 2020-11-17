8605950f4fa3a9f77e0180bcdc2de6da49a1b207
Kevin McNeil <dogwoodrawks@gmail.com>
2020-11-16 18:58:35 -0800

    lab 26

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app.jsx';
+
+let root = document.getElementById('root');
+ReactDOM.render(
+  <>
+    <App />
+  </>
+  , root);
